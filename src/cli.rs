3e5a2575ff3370a3963e5ed17f4ab54e12c4725b
Jacob Michels <jacob.michels2025@gmail.com>
2024-04-26 22:57:58 -0400

    accept tailscale oauth args

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,20 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use redact::Secret;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
     #[arg(short, long, default_value = "policies")]
     pub policies_dir: PathBuf,
+
+    #[arg(short, long)]
+    pub tailscale_client_id: String,
+
+    #[arg(short, long)]
+    pub tailscale_client_secret: Secret<String>,
+
+    #[arg(short, long)]
+    pub tailscale_token_url: String,
 }
