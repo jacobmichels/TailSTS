6c6f45689e706a09ac54a5de844add145b1d2efd
Jacob Michels <jacob.michels2025@gmail.com>
2024-04-28 13:21:14 -0400

    add alg, issuer is an array

--- src/policy.rs
+++ src/policy.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use jsonwebtoken::jwk::JwkSet;
+use jsonwebtoken::{jwk::JwkSet, Algorithm};
 use serde::Deserialize;
 
 // This file contains Policy struct definitions used throughout the program.
@@ -8,17 +8,28 @@ use serde::Deserialize;
 // A policy without its JWKS loaded
 #[derive(Deserialize, Debug)]
 pub struct LocalPolicy {
-    pub issuer: String,
+    pub issuer: HashSet<String>,
     pub subject: String,
+    pub algorithm: String,
     pub jwks_url: String,
     pub permissions: HashMap<String, String>,
 }
 
 impl LocalPolicy {
     pub fn attach_jwks(self, jwks: JwkSet) -> PolicyWithJWKS {
+        let alg = match self.algorithm.to_lowercase().as_str() {
+            "rs256" => Algorithm::RS256,
+            "rs384" => Algorithm::RS384,
+            "rs512" => Algorithm::RS512,
+            _ => {
+                panic!("invalid policy") // TODO: I can do better...
+            }
+        };
+
         PolicyWithJWKS {
             issuer: self.issuer,
             jwks,
+            algorithm: alg,
             permissions: self.permissions,
             subject: self.subject,
         }
@@ -28,8 +39,9 @@ impl LocalPolicy {
 // A policy without its JWKS loaded
 #[derive(Deserialize, Debug, Clone)]
 pub struct PolicyWithJWKS {
-    pub issuer: String,
+    pub issuer: HashSet<String>,
     pub subject: String,
+    pub algorithm: Algorithm,
     pub jwks: JwkSet,
     pub permissions: HashMap<String, String>,
 }
