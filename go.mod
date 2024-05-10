548a8c8f85ea5a9e2dc07371512c72fd56d72550
Jacob Michels <jacob.michels2025@gmail.com>
2024-05-09 22:43:13 -0400

    restart with go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jacobmichels/tail-sts
+
+go 1.22.3
--- src/policy/mod.rs
+++
@@ -1,7 +0,0 @@
-mod algorithm;
-mod local_policy;
-mod policy_with_jwks;
-
-pub use algorithm::ACCEPTABLE_ALGORITHMS;
-pub use local_policy::LocalPolicy;
-pub use policy_with_jwks::PolicyWithJWKS;
