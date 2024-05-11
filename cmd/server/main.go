9a20a4332e483f7cfa808b117189969874fcf3d1
Jacob Michels <jacob.michels2025@gmail.com>
2024-05-11 11:34:54 -0400

    valid that token WOO

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,8 +6,10 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"slices"
 	"strings"
 
+	"github.com/MicahParks/jwkset"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/jacobmichels/tail-sts/pkg/policy"
 )
@@ -16,8 +18,9 @@ func main() {
 	ctx := context.Background()
 
 	policies := getPolicies(ctx)
+	decider := GateKeeper{Policies: policies}
 
-	err := startServer(policies)
+	err := startServer(policies, decider)
 	if err != nil {
 		panic(err)
 	}
@@ -43,17 +46,18 @@ type Request struct {
 	Scopes map[string]string
 }
 
-type Decider struct {
+type GateKeeper struct {
 	Policies []policy.Policy
 }
 
-func (d *Decider) Decide(ctx context.Context, req Request, token jwt.Token) (bool, error) {
-
+func (d *GateKeeper) Evaluate(ctx context.Context, req Request, token jwt.Token) (bool, error) {
+	return false, nil
 }
 
-func startServer(policies []policy.Policy, decider Decider) error {
+func startServer(policies []policy.Policy, decider GateKeeper) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+		// perform basic validation of the format of the request
 		auth := r.Header.Get("Authorization")
 		if auth == "" {
 			http.Error(w, "missing Authorization header", http.StatusUnauthorized)
@@ -65,36 +69,6 @@ func startServer(policies []policy.Policy, decider Decider) error {
 			return
 		}
 
-		// It's possible to use jwt.ParseUnverified to parse the token without verifying the signature
-		parser := jwt.NewParser()
-		var claims jwt.RegisteredClaims
-		parser.ParseUnverified(string(auth[7:]), &claims)
-
-		token, err := jwt.Parse(string(auth[7:]), func(token *jwt.Token) (interface{}, error) {
-			if token.Method.Alg() != jwt.SigningMethodRS256.Name {
-				return nil, jwt.ErrInvalidKey
-			}
-
-			kid := token.Header["kid"].(string)
-			if kid == "" {
-				return nil, jwt.ErrInvalidKey
-			}
-
-			policy, err := matchPolicy(r.Context(), policies, kid)
-			if err != nil {
-				return nil, fmt.Errorf("failed to match policy: %w", err)
-			}
-			if policy == nil {
-				return nil, errors.New("no matching policy")
-			}
-
-			return policy.Jwks.Keyfunc(token)
-		})
-		if err != nil {
-			http.Error(w, "invalid token", http.StatusUnauthorized)
-			return
-		}
-
 		var req Request
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
@@ -102,7 +76,40 @@ func startServer(policies []policy.Policy, decider Decider) error {
 			return
 		}
 
-		w.WriteHeader(http.StatusOK)
+		// parse the token without validating it
+		// this is needed to read the issuer in order to find a matching policy
+		parser := jwt.NewParser()
+		var claims jwt.RegisteredClaims
+		_, _, err = parser.ParseUnverified(string(auth[7:]), &claims)
+		if err != nil {
+			http.Error(w, "invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		// find the policy that matches the token's issuer
+		policy := findByIssuer(r.Context(), policies, claims.Issuer)
+		if policy == nil {
+			http.Error(w, "no matching policy", http.StatusUnauthorized)
+			return
+		}
+
+		// use that policy's JWKs to verify the token
+		token, err := jwt.Parse(string(auth[7:]), func(token *jwt.Token) (any, error) {
+			return policy.Jwks.Keyfunc(token)
+		}, jwt.WithValidMethods([]string{policy.Algorithm}))
+
+		switch {
+		case token.Valid:
+			w.Write([]byte("OK"))
+		case errors.Is(err, jwt.ErrTokenMalformed):
+			http.Error(w, "malformed token", http.StatusUnauthorized)
+		case errors.Is(err, jwt.ErrTokenSignatureInvalid):
+			http.Error(w, "invalid signature", http.StatusUnauthorized)
+		case errors.Is(err, jwt.ErrTokenExpired) || errors.Is(err, jwt.ErrTokenNotValidYet):
+			http.Error(w, "token expired or not yet valid", http.StatusUnauthorized)
+		default:
+			http.Error(w, "cannot handle this token", http.StatusUnauthorized)
+		}
 	})
 
 	srv := http.Server{Addr: ":8080", Handler: mux}
@@ -110,19 +117,28 @@ func startServer(policies []policy.Policy, decider Decider) error {
 	return srv.ListenAndServe()
 }
 
-func matchPolicy(ctx context.Context, policies []policy.Policy, kid string) (*policy.Policy, error) {
+func findByIssuer(ctx context.Context, policies []policy.Policy, issuer string) *policy.Policy {
 	for _, policy := range policies {
-		jwks, err := policy.Jwks.Storage().KeyReadAll(ctx)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read keys: %w", err)
+		if slices.Contains(policy.Issuers, issuer) {
+			return &policy
 		}
+	}
 
-		for _, jwk := range jwks {
-			if jwk.Marshal().KID == kid {
-				return &policy, nil
-			}
+	return nil
+}
+
+func findKeyForKID(ctx context.Context, policy policy.Policy, kid string) (*jwkset.JWK, error) {
+	keys, err := policy.Jwks.Storage().KeyReadAll(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read keys: %w", err)
+	}
+
+	for _, key := range keys {
+		if key.Marshal().KID == kid {
+			return &key, nil
 		}
 	}
 
 	return nil, nil
+
 }
